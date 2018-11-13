0c99682a2099e79f258f998057a7ef3e2c731a47
uncleseneca <uncleseneca@gmail.com>
2018-11-13 07:43:05 +0300

    prototype fix

--- src/prototype/index.ts
+++ src/prototype/index.ts
@@ -1,13 +1,23 @@
 export class Application {
   private shapes: Shape[] = []
   constructor() {
-    const circle = new Circle({ x: 0, y: 0, radius: 10, color: 'black' })
+    const circle = new Circle()
+    circle.x = 0
+    circle.y = 0
+    circle.color = 'black'
+    circle.radius = 10
     const circleClone = circle.clone()
 
     this.shapes.push(circle)
     this.shapes.push(circleClone)
 
-    const rectangle = new Rectangle({ x: 1, y: 0, width: 10, height: 10, color: 'green' })
+    const rectangle = new Rectangle()
+    rectangle.x = 1
+    rectangle.y = 1
+    rectangle.width = 10
+    rectangle.height = 10
+    rectangle.color = 'green'
+
     const rectangleClone = rectangle.clone()
 
     this.shapes.push(rectangle)
@@ -24,34 +34,32 @@ export class Application {
 
 abstract class Shape {
 
-  public x: ShapeType['x']
-  public y: ShapeType['y']
-  public color: ShapeType['color']
+  public x: number
+  public y: number
+  public color: string
 
-  constructor(source: ShapeType) {
-    this.x = source.x
-    this.y = source.y
-    this.color = source.color
+  constructor(source: Shape) {
+    if (source) {
+      this.x = source.x
+      this.y = source.y
+      this.color = source.color
+    }
   }
   public abstract clone(): Shape
 
 }
 
-interface ShapeType {
-  x: number,
-  y: number,
-  color: string,
-}
-
 class Rectangle extends Shape {
-  public width: RectangleType['width']
-  public height: RectangleType['height']
+  public width: number
+  public height: number
 
-  constructor(source: RectangleType) {
+  constructor(source?: Rectangle) {
     super(source)
 
-    this.width = source.width
-    this.height = source.height
+    if (source) {
+      this.width = source.width
+      this.height = source.height
+    }
   }
 
   public clone() {
@@ -59,20 +67,18 @@ class Rectangle extends Shape {
   }
 }
 
-type RectangleType = ShapeType & { width: number, height: number }
-
 class Circle extends Shape {
-  public radius: CircleType['radius']
+  public radius: number
 
-  constructor(source: CircleType) {
+  constructor(source?: Circle) {
     super(source)
 
-    this.radius = source.radius
+    if (source) {
+      this.radius = source.radius
+    }
   }
 
   public clone() {
     return new Circle(this)
   }
 }
-
-type CircleType = ShapeType & { radius: number }
